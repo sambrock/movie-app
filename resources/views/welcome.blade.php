8cfb3611d173babe2a5912a746957441ca88094c
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-07-04 01:37:45 +0100

    Initial commit.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <title>Test</title>
 
         <!-- Fonts -->
@@ -14,4 +14,5 @@
         hi
         {{ $testVar }}
     </body>
+    <script src="{{ asset('js/main.js') }}"></script>
 </html>
