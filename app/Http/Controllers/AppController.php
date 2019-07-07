3783faadbc572e4e920103f6293835298849948d
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-07-07 02:04:08 +0100

    Added test functions.

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tmdb\Laravel\Facades\Tmdb;
 
 class AppController extends Controller
 {
     public function test()
     {
-        $testVar = "Hello world";
-        return view('welcome')->with('testVar', $testVar);
-
+        //$movie = Tmdb::getMoviesApi()->getMovie(100);
+        $movies = Tmdb::getSearchApi()->searchMovies('avengers');
+        $results = $movies["results"];
+        return view('filmist/index', ['results' => $results]);
     }
 }
