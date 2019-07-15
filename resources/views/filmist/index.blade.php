3d27c3bb1698a939ff04bdae82f07e752705c6dd
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-07-15 17:56:38 +0100

    Implemented movie search for log.

--- resources/views/filmist/index.blade.php
+++ resources/views/filmist/index.blade.php
@@ -4,13 +4,32 @@
 
 @section('content')
 
+<div class="sub-nav-wrapper">
 
+</div>
 
-@foreach($results as $key=>$movie)
-    <img src="https://image.tmdb.org/t/p/w500{{$movie['poster_path']}}">
-    @php
-    ++$key
-    @endphp
-@endforeach
+<div class="poster-list watched">
+    @foreach($results as $movie)
+    <div class="poster-container">
+        <div class="poster-img-container">
+            <img src="https://image.tmdb.org/t/p/w500{{$movie['poster_path']}}" class="poster-img">
+        </div>
+    </div>
+    @endforeach
+
+</div>
+
+<form action="{{url('log')}}" method="POST">
+    {{ csrf_field() }}
+    <label for="name">Name:</label>
+    <input type="text" id="movie-search" name="name">
+    <label for="rating">Rating:</label>
+    <input type="text" id="rating" name="rating">
+    <input type="hidden" id="movie_id" name="movie_id" value="">
+    <input type="submit" name="submitBtn" value="Add Film">
+</form>
+<div class="ac-results">
+
+</div>
 
 @endsection
