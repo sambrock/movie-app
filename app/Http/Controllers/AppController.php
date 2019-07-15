cd1e1ac14699908f1c6ca7adefd9cf73ce10b871
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-07-15 17:57:05 +0100

    Added search function and save log function.

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -4,14 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Tmdb\Laravel\Facades\Tmdb;
+use App\Seen;
+use Auth;
+use Carbon\Carbon;
 
 class AppController extends Controller
 {
     public function test()
     {
-        //$movie = Tmdb::getMoviesApi()->getMovie(100);
-        $movies = Tmdb::getSearchApi()->searchMovies('avengers');
+        $movies = Tmdb::getMoviesApi()->getRecommendations(18);
         $results = $movies["results"];
         return view('filmist/index', ['results' => $results]);
+    }
+    public function search($searchTerm)
+    {
+        $movies = Tmdb::getSearchApi()->searchMovies($searchTerm);
+        $results = $movies["results"];
+        return response()->json(['results' => $results]);
+    }
+    public function log(Request $request)
+    {
+        $user_id = Auth::id();
+
+        $seen = new Seen();
+        $seen->user_id = $user_id;
+        $seen->movie_id = $request->movie_id;
+        $seen->rating = $request->rating;
+        $seen->save();
+
+        return redirect('/');
+    }
+    public function seen()
+    {
+        $user_id = Auth::id();
+        $movies = Seen::where('user_id', $user_id)->get();
+
+        $results = [];
+
+        foreach($movies as $movie)
+        {
+            $result = Tmdb::getMoviesApi()->getMovie($movie['movie_id']);
+            array_push($results, $result);
+        }
+
+        return view('filmist/seen', ['results' => $results]);
     }
 }
