0841823e1e1ff66d165ce5e2f87d100ea9e0b1d8
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-07-17 17:03:12 +0100

    Added menu animation.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,30 @@
-$(function () { $('#movie-search').keyup(function() {
+function getPopularMovies(){
+    $.ajax({
+        method: 'get',
+        url: 'popular',
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+            if(data.results != 0){
+                $movies = '<div class="poster-container-head">';
+                $.each(data.results, function(index, movie) {
+                    if(index < 6){
+                        $movies += '<div class="poster-container"><div class="poster-img-container"><img src="https://image.tmdb.org/t/p/w500'+movie.poster_path+'" class="poster-img"></div></div>';
+                    }
+
+                });
+                $movies += '</div>';
+                $('.popular-films').html($movies);
+            }
+        }
+    });
+}
+
+$( document ).ready(function() {
+    getPopularMovies();
+});
+
+$('#movie-search').keyup(function() {
     //    if($(this).val().length > 1){
     //        $searchTerm = $(this).val();
     //        $(".patient").hide();
@@ -35,9 +61,20 @@ $(function () { $('#movie-search').keyup(function() {
         }
     });
 });
-              });
+
 
 $(document).on('click', '.ac-results ul li', function() {
     $movie_id = $(this).attr("data-id");
     $('#movie_id').val($movie_id);
+});
+
+$('#menu-btn').on('click', function(){
+    $('#nav').show(400);
+    $('#nav .content').slideDown(400);
+});
+
+$('#menu-btn-active').on('click', function(){
+    console.log("click");
+    $('#nav').fadeOut(200);
+    $('#nav .content').fadeOut(200);
 });
