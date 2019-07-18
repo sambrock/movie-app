902df3ff714150efcf76d19254277af39d7dd4eb
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-07-19 00:53:56 +0100

    Added animation elements.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,43 +3,42 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Cmpatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://unpkg.com/@barba/core"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <link href="{{asset('/css/style.css')}}" rel="stylesheet" type="text/css">
         <link href="{{asset('/css/all.css')}}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
         <title>@yield('title')</title>
     </head>
     <body>
+
         <header>
-            <a class="nav-icons" id="menu-btn"><i class="fa fa-bars"></i></a>
             <img id="logo" src="{{asset('/img/filmlist-logo.svg')}}" height="35">
-            <a class="nav-icons" id="log-btn"><i class="fa fa-plus"></i></a>
+            <nav>
+                <ul>
+                    <li><a href="{{ url('/seen') }}">Seen</a></li>
+                    <li><a href="{{ url('/') }}">Lists</a></li>
+                    <li><a href="{{ url('/watchlist') }}">Watchlist</a></li>
+                </ul>
+            </nav>
+            <a id="log-btn"><i class="fa fa-plus"></i>Log</a>
         </header>
-        <div id="nav">
-            <div class="menu-bg"></div>
-            <div class="content">
-                <div class="head">
-                    <a class="nav-icons" id="menu-btn-active"><i class="fa fa-bars"></i></a>
-                    <img id="logo" src="{{asset('/img/filmlist-logo-blk.svg')}}" height="35">
-                    <a class="nav-icons" id="log-btn active"><i class="fa fa-plus"></i></a>
+        <div class="screen-wipe"></div>
+        <div id="barba-wrapper">
+            <div class="barba-container">
+                <div class="container">
+                    @yield('content')
                 </div>
-                <div class="links">
-                    <ul>
-                        <li><a href="{{ url('/') }}">Home</a></li>
-                        <li><a href="{{ url('/') }}">Lists</a></li>
-                        <li><a href="{{ url('/seen') }}">Seen</a></li>
-                        <li><a href="{{ url('/watchlist') }}">Watchlists</a></li>
-                    </ul>
+                <div class="wipe-bg transition-wipe">
+
                 </div>
-                <div class="popular-films">
-                    <h3>Popular films</h3>
-                </div>
+                <script src="{{asset('/js/color-thief.min.js')}}" type="text/javascript"></script>
+                <script src="{{asset('/js/barba.min.js')}}" type="text/javascript"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+                <script src="{{asset('/js/main.js')}}"></script>
             </div>
         </div>
 
-        <div class="container">
-            @yield('content')
-        </div>
-        <script src="{{asset('/js/main.js')}}"></script>
     </body>
 </html>
